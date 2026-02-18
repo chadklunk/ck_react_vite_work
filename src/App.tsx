c87cbd76061537002a4a89bbf0af80ba9941d557
Chad Klunk <chad.klunk@gmail.com>
2026-02-17 20:10:16 -0600

    Updated base config and app files

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- 
-
   return (
-    <>
     <div className="app">
       <div className="navbar">
         <div className="navbar-container">
-        <div><a href='/'>home</a></div>
-        <div><a href='/'>about</a></div>
-        <div><a href='/'>portfolio</a></div>
-        <div><a href='/'>pricing</a></div>
+          <div><a href="/">home</a></div>
+          <div><a href="/">about</a></div>
+          <div><a href="/">portfolio</a></div>
+          <div><a href="/">pricing</a></div>
         </div>
       </div>
+
       <div className="page">
-        <div className="ck_lead_in">welcome to the portfolio of</div>
-        <div className="bruno-ace-sc-regular"><div>C H A D</div> <div>K L U N K</div> </div>      
-        <p/>
-        <div>            
-            <div className="theK">K</div>
-        </div>  
-      </div>     
-      <footer className="footer">Footer content</footer>
-    </div>  
-      {/*<footer style={{height:"10vh", background: "linear-gradient(to right, yellow, orangered)",}}>Footer content</footer>*/}
-      
-    </>
+        <div className="ck_lead_in">
+          welcome to the portfolio of
+        </div>
+
+        <div className="bruno-ace-sc-regular">
+          <div>C H A D</div>
+          <div>K L U N K</div>
+        </div>
+
+        <div className="theK">K</div>
+      </div>
+
+      <footer className="footer">
+        Footer content
+      </footer>
+    </div>
   )
 }
 
 export default App
+
